a6f37e9f2e7c8859927d2a3ed5ed398e0fd1d409
Reese Ford <reese_ford1@baylor.edu>
2022-10-10 12:04:01 -0500

    Manual steps 1 and 2

--- modules/buttonNew.py
+++ modules/buttonNew.py
@@ -4,9 +4,29 @@ def button():
     # Vars
     color = "EMPTY"
     text = "EMPTY"
+    instruction = "EMPTY"
     global batteries
     global litIndicators
 
     # Logic
+    #Starting Information
 
+    color = input("What is the color of the button? (blu, w, y, r, other): ")
 
+    #Manual Step 1
+    if color == "b":
+        if text == "EMPTY":
+            text = input("What does the button say? (all lowercase): ")
+        if text == "abort":
+            instruction = "hold"
+
+    #Manual Step 2
+    if batteries == "EMPTY":
+        batteries = input("How many batteries are on the bomb?: ")
+    if batteries > 1:
+        if text == "EMPTY":
+            text = input("What does the button say? (all lowercase): ")
+        if text == "detonate":
+            instruction = "pressRelease"
+
+    
