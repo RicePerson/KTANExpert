ab0f8f14985accf4418afbc541072f8c469e12f7
Reese Ford <reese_ford1@baylor.edu>
2022-10-10 11:47:11 -0500

    Added global "serialNum" and changed over wires.py

--- KTANExpert.py
+++ KTANExpert.py
@@ -23,6 +23,8 @@ from modules.wireSeq import *
 isDoing = True
 explode = False
 module = "EMPTY"
+serialNum = "EMPTY"
+
 print("Incase you haven't launched the game before, the verification code as of 10/9/2022 is 241")
 
 #Module Loop
