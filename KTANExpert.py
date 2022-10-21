181c1d106c781d7a690c40c3dab57537030ab9de
Reese Ford <reese_ford1@baylor.edu>
2022-10-21 11:06:56 -0500

    Moved the morse word lists into the module definition

--- KTANExpert.py
+++ KTANExpert.py
@@ -1006,25 +1006,25 @@ def morseToAlpha(morseIn):
             print("Error. Invaild morse, '" + str(morseIn) + "'. Resetting")
             return "ERROR"
 
-shell = ["S", "H", "E", "L", "L"]
-halls = ["H", "A", "L", "L", "S"]
-slick = ["S", "L", "I", "C", "K"]
-trick = ["T", "R", "I", "C", "K"]
-boxes = ["B", "O", "X", "E", "S"]
-leaks = ["L", "E", "A", "K", "S"]
-strobe = ["S", "T", "R", "O", "B", "E"]
-bistro = ["B", "I", "S", "T", "R", "O"]
-flick = ["F", "L", "I", "C", "K"]
-bombs = ["B", "O", "M", "B", "S"]
-bbreak = ["B", "R", "E", "A", "K"]
-brick = ["B", "R", "I", "C", "K"]
-steak = ["S", "T", "E", "A", "K"]
-sting = ["S", "T", "I", "N", "G"]
-vector = ["V", "E", "C", "T", "O", "R"]
-beats = ["B", "E", "A", "T", "S"] 
-
 #Morse Code Logic (input -> output)
 def morse():
+    shell = ["S", "H", "E", "L", "L"]
+    halls = ["H", "A", "L", "L", "S"]
+    slick = ["S", "L", "I", "C", "K"]
+    trick = ["T", "R", "I", "C", "K"]
+    boxes = ["B", "O", "X", "E", "S"]
+    leaks = ["L", "E", "A", "K", "S"]
+    strobe = ["S", "T", "R", "O", "B", "E"]
+    bistro = ["B", "I", "S", "T", "R", "O"]
+    flick = ["F", "L", "I", "C", "K"]
+    bombs = ["B", "O", "M", "B", "S"]
+    bbreak = ["B", "R", "E", "A", "K"]
+    brick = ["B", "R", "I", "C", "K"]
+    steak = ["S", "T", "E", "A", "K"]
+    sting = ["S", "T", "I", "N", "G"]
+    vector = ["V", "E", "C", "T", "O", "R"]
+    beats = ["B", "E", "A", "T", "S"] 
+
     #Def Vars
     userMorseUnsplit = str(input("What is your Morse Code (see README.md for instructions)?: "))
     if userMorseUnsplit == "...": # Exit Command
